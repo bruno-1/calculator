aaa327505a691dfbc86f77eec3bc879762490005
Bruno Serafini <bruno.serafini@pm.me>
2024-04-30 22:32:14 -0300

    feat: add button equals

--- script.js
+++ script.js
@@ -3,9 +3,11 @@ let display = "";
 const btns = document.getElementsByTagName("button");
 for (let btn of btns) {
   btn.addEventListener("click", (e) => {
-    display += btn.textContent;
-  if (btn.textContent === "CE") display = "";
-
-    document.getElementById("display").textContent = display;
+    if (btn.textContent !== "=") {
+      display += btn.textContent;
+      if (btn.textContent === "CE") display = "";
+  
+      document.getElementById("display").textContent = display;
+    }
   });
 }
