92081f9cf05be71ac0dfe6d1228a6b0ca875a77e
Bruno Serafini <bruno.serafini@pm.me>
2024-05-01 13:40:45 -0300

    fix: The calculator should not evaluate more than a single pair of numbers at a time

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let display = "";
+const OPERATORS = ["*", "/", "+", "-"];
 
 function operate(operator, n1, n2) {
   if (operator === "*") return n1 * n2;
@@ -10,15 +11,24 @@ function operate(operator, n1, n2) {
 const btns = document.getElementsByTagName("button");
 for (let btn of btns) {
   btn.addEventListener("click", (e) => {
+    if (
+      OPERATORS.includes(btn.textContent) &&
+      (
+        (!display) ||
+        OPERATORS.includes(display.charAt(display.length -1)) ||
+        OPERATORS.some(el => display.includes(el)))
+    ) {
+      return;
+    }
+
     if (btn.textContent !== "=") {
       display += btn.textContent;
       if (btn.textContent === "CE") display = "";
     } else {
-      const operators = ["*", "/", "+", "-"];
       let operator = "+";
       let n1 = 0;
       let n2 = 0;
-      for (let operatorItem of operators) {
+      for (let operatorItem of OPERATORS) {
         if (display.indexOf(operatorItem) !== -1) {
           operator = display.charAt(display.indexOf(operatorItem));
           n1 = Number(display.substring(0, display.indexOf(operatorItem)));
